04d899740471cd0e541ac826f8e0421b38c9b727
Aleksandr <afanasiev.aleksandr.92@gmail.com>
2017-10-17 20:02:32 +0300

    first commit

---
+++ CoreDataSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataSample
+//
+//  Created by Aleksandr Afanasiev on 16.10.17.
+//  Copyright © 2017 Aleksandr Afanasiev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
